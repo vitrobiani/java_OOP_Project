b240e7f129093e64249f527280815fa7049087b0
vitrobiani <vitrobiani@gmail.com>
2024-03-10 20:12:44 +0200

    Changes by Dolev Telem

---
+++ src/javaOOPProject/hw3_Answer.java
@@ -0,0 +1,73 @@
+package javaOOPProject;
+
+public class hw3_Answer {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private int AnswerIndex;
+    protected boolean isCorrect;
+
+    public enum eType {
+        open, close
+    };
+
+    protected AnswerAdapter.eType type;
+    private AnswerRepository AR;
+
+    // for the closed questions - need to set isCorrect
+    public hw3_Answer(int AnsI, boolean isCorrect, AnswerRepository AR, AnswerAdapter.eType type) {
+        AnswerIndex = AnsI;
+        this.isCorrect = isCorrect;
+        this.AR = AR;
+        this.type = type;
+    }
+
+    // for the open question - school answer is always true
+    public hw3_Answer(int AnsI, AnswerRepository AR, AnswerAdapter.eType type) {
+        AnswerIndex = AnsI;
+        this.AR = AR;
+        this.type = type;
+        this.isCorrect = true;
+    }
+
+    public int hw3_getAnswerIndex() {
+        return AnswerIndex;
+    }
+
+    public boolean hw3_isCorrect() {
+        return isCorrect;
+    }
+
+    public void hw3_setIsCorrect(boolean bool) {
+        isCorrect = bool;
+    }
+
+    public hw3_Answer.eType hw3_getType() {
+        return type;
+    }
+
+    public AnswerRepository hw3_getAR() {
+        return AR;
+    }
+
+    public String hw3_getAnswer() {
+        return AR.getRepo(AnswerIndex);
+    }
+
+    public String hw3_toString() {
+        StringBuffer sB = new StringBuffer();
+        sB.append("the answer: " + AR.getRepo(AnswerIndex)
+                + '\n' + "it is: " + (isCorrect) != null ? "correct": "incorrect"
+                + '\n' + "its type is: " + type);
+
+        return sB.toString();
+    }
+
+    public boolean hw3_equals(AnswerAdapter a) {
+        if (a.type == type && a.getAnswerIndex() == AnswerIndex && a.getType() == type) {
+            return true;
+        }
+        return false;
+    }
+}
